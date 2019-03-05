f18465cc53f9f7053f593f14e7ff28e6f744b949
frankqpfu <wit.zland@hotmail.com>
2019-03-05 18:42:02 +0800

    Add files via upload
    test

---
+++ main.js
@@ -0,0 +1,50 @@
+
+require.config({
+    paths: {
+        "rtmp-streamer": "../rtmp-streamer.min"
+    }
+});
+
+require(["rtmp-streamer"], function (RtmpStreamer) {
+
+    var getUrl = function () {
+        return document.getElementById('url').value;
+    };
+
+    var getName = function () {
+        return document.getElementById('stream-name').value;
+    };
+    
+    var getUrl2 = function () {
+        return document.getElementById('url2').value;
+    };
+
+    var getName2 = function () {
+        return document.getElementById('stream-name2').value;
+    };
+
+    var streamer = new RtmpStreamer(document.getElementById('rtmp-streamer'));
+    var player = new RtmpStreamer(document.getElementById('rtmp-player'));
+    
+    streamer.setScreenSize(640, 480);
+    streamer.setMicQuality(6);
+    player.setScreenSize(640,480);
+    
+    document.getElementById("publish").addEventListener("click", function () {
+        streamer.publish(getUrl(), getName());
+    });
+    
+    document.getElementById("play").addEventListener("click", function () {
+        player.play(getUrl2(), getName2());
+    });
+    
+
+    document.getElementById("streamer-disconnect").addEventListener("click", function () {
+        streamer.disconnect();
+    });
+
+    document.getElementById("player-disconnect").addEventListener("click", function () {
+        player.disconnect();
+    });
+
+});
